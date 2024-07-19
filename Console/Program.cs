656e0cfcfe520555dbb5ac9d54eeeddb3386d405
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-19 03:35:32 +0300

    Constructor Practice Completed

--- Console/Program.cs
+++ Console/Program.cs
@@ -11,13 +11,5 @@ Person person = new Person()
 //nesnenin propertylerini yazdırma
 Console.WriteLine($"{person.FirstName} {person.LastName} {person.BirthDate}");
 
-/* 
- Person person = new Person()
-{
-    FirstName = "Adıgüzel",
-    LastName = "Yusufcan",
-    BirthDate = DateTime.Parse("09-07-1998"),
-};
-
-Console.WriteLine($"{person.FirstName} {person.LastName} {person.BirthDate}");
- */
+//instance olusturma ve ctor ile deger atama
+Baby baby = new Baby(firstName: "Yusufcan", lastName: "Adıgüzel");
---
+++ Entities/Baby.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Baby
+    {
+        //dogum tarihi
+        public DateTime BirthDate { get; set; }
+        //ad
+        public string? FirstName { get; set; }
+        //soyad
+        public string? LastName { get; set; }
+
+        //default constructor
+        public Baby()
+        {
+            BirthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        //dogum tarihi, ad ve soyad alanlarini instance olustururken atama
+        public Baby(string firstName, string lastName) : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
