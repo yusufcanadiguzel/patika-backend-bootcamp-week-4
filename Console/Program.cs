6b10299b1554f5f46bba52056c4116c7162e5f66
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-19 03:26:49 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+
+//nesne ornegi olusturma
+Person person = new Person()
+{
+    FirstName = "Yusufcan",
+    LastName = "Adıgüzel",
+    BirthDate = DateTime.Parse("09-07-1998"),
+};
+
+//nesnenin propertylerini yazdırma
+Console.WriteLine($"{person.FirstName} {person.LastName} {person.BirthDate}");
+
+/* 
+ Person person = new Person()
+{
+    FirstName = "Adıgüzel",
+    LastName = "Yusufcan",
+    BirthDate = DateTime.Parse("09-07-1998"),
+};
+
+Console.WriteLine($"{person.FirstName} {person.LastName} {person.BirthDate}");
+ */
---
+++ Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Person
+    {
+        //ad
+        public string? FirstName { get; set; }
+        //soyad
+        public string? LastName { get; set; }
+        //dogum tarihi
+        public DateTime BirthDate { get; set; }
+    }
+}
