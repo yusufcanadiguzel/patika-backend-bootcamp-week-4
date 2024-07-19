d51b1b11bedc8fd5531d756efe722245b40ba602
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-19 14:17:50 +0300

    Patika Library App Completed

---
+++ PatikaLibraryApp/Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibraryApp
+{
+    internal class Book
+    {
+        private string? _title;
+        private string? _author;
+        private int _pageCount;
+        private string? _publisher;
+        private DateTime _publishDate;
+
+        public string? Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string? Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public int PageCount
+        {
+            get { return _pageCount; }
+            set { _pageCount = value; }
+        }
+
+        public string? Publisher
+        {
+            get { return _publisher; }
+            set { _publisher = value; }
+        }
+
+        public DateTime PublishDate
+        {
+            get { return _publishDate; }
+            set { _publishDate = value; }
+        }
+
+        //yayimlanma tarihine instance anindaki tarihi atar
+        public Book()
+        {
+            PublishDate = DateTime.Now;
+        }
+
+        //isim, yazar, sayfa sayisi ve yayimciyi instance esnasinda atar ve default ctoru calistirir
+        public Book(string name, string author, int pageCount, string publisher) : this() 
+        {
+            Title = name;
+            Author = author;
+            PageCount = pageCount;
+            Publisher = publisher;
+        }
+    }
+}
---
+++ PatikaLibraryApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using PatikaLibraryApp;
+
+//instance olusturma
+Book book = new Book(name: "Adı Aylin", author: "Ayşe Kulin", pageCount: 398, publisher: "Remzi Kitabevi");
+
+//nesne bilgilerini yazdirma
+Console.WriteLine($"Kitap adı: {book.Title}");
+Console.WriteLine($"Yazar: {book.Author}");
+Console.WriteLine($"Sayfa sayısı: {book.PageCount}");
+Console.WriteLine($"Yayımlanma tarihi: {book.PublishDate}");
+Console.WriteLine($"Yayımlayıcı: {book.Publisher}");
+
+//Class: Ihtiyac duyulan nesneleri olusturabilmek icin kullanilan blueprinttir.
+//Property: Class'in field'larini yonetmek ve erisimlerini kontrol etmek icin kullanilir.
+//New: Class'in ram uzerinde bir instance'inin olusturulmasini saglar.
+//Constructor: Objenin instance'sinin olustugu anda calisan method'tur.
