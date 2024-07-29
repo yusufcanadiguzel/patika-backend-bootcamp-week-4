798fa7cee1deda153107573b3754fbb902ba6ce4
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-29 16:17:12 +0300

    Class Directory Changed.

---
+++ Console/Baby.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Baby
+    {
+        private DateTime _birthDate;
+        private string? _firstName;
+        private string? _lastName;
+
+        //dogum tarihi
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+
+        //ad
+        public string? FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        //soyad
+        public string? LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        //default constructor
+        public Baby()
+        {
+            BirthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        //dogum tarihi, ad ve soyad alanlarini instance olustururken atama
+        public Baby(string firstName, string lastName) : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ Console/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace Entities
+{
+    public class Person
+    {
+        private string? _firstName;
+        private string? _lastName;
+        private DateTime _birthDate;
+
+        //ad
+        public string? FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        //soyad
+        public string? LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        //dogum tarihi
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+    }
+}
